0fb8fa7590ad3f2079b9183f9c7aff0130b10ff6
idke64 <deliciousbrain42@gmail.com>
2024-10-19 14:39:56 -0400

    input changed

--- src/app/(pages)/main/page.tsx
+++ src/app/(pages)/main/page.tsx
@@ -1,6 +1,7 @@
 import {
   faArrowUp,
   faMagnifyingGlass,
+  faPaperclip,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
@@ -10,9 +11,15 @@ export default function Main() {
     <section className="page-margins py-4">
       <div className="flex w-full gap-x-4">
         <div className="w-full flex gap-x-2">
-          <input className="bg-surface-200 w-full rounded p-4" />
-          <button className="btn-primary h-full aspect-square">
-            <FontAwesomeIcon className="h-14" icon={faArrowUp} />
+          <div className="bg-surface-200 px-4 py-3 w-full flex items-center gap-x-3 rounded-lg focus-within:ring-1 ring-palette-200 duration-200">
+            <FontAwesomeIcon className="text-2xl" icon={faPaperclip} />
+            <input
+              className="bg-surface-200 w-full focus:outline-none"
+              placeholder="Enter your prompt"
+            />
+          </div>
+          <button className="btn-primary p-0 h-full aspect-square">
+            <FontAwesomeIcon className="text-2xl" icon={faArrowUp} />
           </button>
         </div>
       </div>
